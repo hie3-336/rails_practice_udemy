255bc9b438b15e6c3648800553f0cd4f2189eb89
hie3-336 <tomohiko336@gmail.com>
2024-06-04 00:32:39 +0900

    10-11

---
+++ String.rb
@@ -0,0 +1,17 @@
+p '文字'.class
+p 'sample'.upcase
+p '文字列です'.slice(0,3)
+p '文字列です'.slice(3,2)
+upper_str = 'sample'.upcase
+p upper_str
+p 'Sample'.downcase
+p 'Sample'.size
+p '100'.class
+p '100'.to_i.class
+p '10.5'.to_f.class
+p 'もじ' + '連結'
+p 'もじ' << '連結'
+p '文字' * 10
+two = '二'
+p "1 + 1 は#{two}"
+p "1 + 1 は#{1+1}"
---
+++ Symbol.rb
@@ -0,0 +1,12 @@
+p :name.class
+str1 = 'str'
+str2 = 'str'
+p str1.object_id
+p str2.object_id
+sym1 = :str
+sym2 = :str
+p sym1.object_id
+p sym2.object_id
+
+h = { name: 'コジマ', prefecture: '東京都' }
+p h
